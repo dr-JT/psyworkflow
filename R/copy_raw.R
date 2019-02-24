8347952d701849e4df5f4a8cd3771127893634ac
Jason Tsukahara <tsuky19@gmail.com>
2019-02-24 01:00:08 -0500

    fix

--- R/copy_raw.R
+++ R/copy_raw.R
@@ -16,21 +16,23 @@ copy_raw <- function(from, to, pattern, subj.list = NULL, remove = FALSE){
   dirs <- list.dirs(from, full.names = FALSE, recursive = FALSE)
   for (task_dir in dirs){
     files <- list.files(paste(from, task_dir, sep = "/"), pattern = pattern, recursive = TRUE, full.names = TRUE)
-    task <- stringr::str_split(task_dir, ". ")
-    if (length(task[[1]])==1){
-      task <- task[[1]][1]
-    } else {
-      task <- task[[1]][2]
-    }
-    copy_dir <- paste(to, task, sep = "/")
-    if (dir.exists(copy_dir)==TRUE){
-      file.copy(files, copy_dir, copy.date = TRUE)
-    } else {
-      dir.create(copy_dir)
-      file.copy(files, copy_dir, copy.date = TRUE)
-    }
-    if (remove==TRUE){
-      file.remove(files)
+    if (length(files)>0){
+      task <- stringr::str_split(task_dir, ". ")
+      if (length(task[[1]])==1){
+        task <- task[[1]][1]
+      } else {
+        task <- task[[1]][2]
+      }
+      copy_dir <- paste(to, task, sep = "/")
+      if (dir.exists(copy_dir)==TRUE){
+        file.copy(files, copy_dir, copy.date = TRUE)
+      } else {
+        dir.create(copy_dir)
+        file.copy(files, copy_dir, copy.date = TRUE)
+      }
+      if (remove==TRUE){
+        file.remove(files)
+      }
     }
   }
 }
