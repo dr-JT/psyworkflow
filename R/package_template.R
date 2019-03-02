a6a63d444cc4dc66a1d701aac2381d694a7e64ef
Jason Tsukahara <tsuky19@gmail.com>
2019-03-02 17:42:49 -0500

    add path

--- R/package_template.R
+++ R/package_template.R
@@ -5,5 +5,5 @@ package_template <- function(path, type, sessions, figures.dir, manuscript.dir,
 
   create_study(type = type, sessions = sessions, figures.dir = figures.dir,
                manuscript.dir = manuscript.dir, presentations.dir = presentations.dir,
-               templates = templates, gf = gf, wmc = wmc, ac = ac)
+               templates = templates, gf = gf, wmc = wmc, ac = ac, path = path)
 }
