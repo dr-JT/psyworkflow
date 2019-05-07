36d0dbdc6c8b5bf3c77dfd34826d912d54536cb0
Jason Tsukahara <tsuky19@gmail.com>
2019-05-07 10:27:29 -0400

    update template

--- R/package_template.R
+++ R/package_template.R
@@ -1,10 +1,10 @@
 package_template <- function(path, type, sessions, generic, gf, wmc, ac,
-                             messy.dir, scored.dir, results.dir, manuscript.dir) {
+                             figures.dir, manuscript.dir, presentations.dir) {
   # ensure path exists
   dir.create(path, recursive = TRUE, showWarnings = FALSE)
 
-  create_study(type = type, sessions = sessions, messy.dir = messy.dir,
-               scored.dir = scored.dir, manuscript.dir = manuscript.dir,
-               results.dir = results.dir, generic = generic,
-               gf = gf, wmc = wmc, ac = ac, path = path, messy.name = "E-Merge")
+  create_study(type = type, sessions = sessions, figures.dir = figures.dir,
+               manuscript.dir = manuscript.dir,
+               presentations.dir = presentations.dir, generic = generic,
+               gf = gf, wmc = wmc, ac = ac, path = path)
 }
