869cb8a4f78f2e93c92a2b3838db4e2a270aaa46
Jason Tsukahara <tsuky19@gmail.com>
2019-05-31 15:28:22 -0400

    Update parameters

--- R/project_template.R
+++ R/project_template.R
@@ -1,10 +1,12 @@
-project_template <- function(path, type, sessions, generic, gf, wmc, ac,
+project_template <- function(path, type, sessions, masterscript, rawscript,
+                             scorescript, mergescript,
                              figures.dir, manuscript.dir, presentations.dir) {
   # ensure path exists
   dir.create(path, recursive = TRUE, showWarnings = FALSE)
 
   create_study(type = type, sessions = sessions, figures.dir = figures.dir,
                manuscript.dir = manuscript.dir,
-               presentations.dir = presentations.dir, generic = generic,
-               gf = gf, wmc = wmc, ac = ac, path = path)
+               presentations.dir = presentations.dir, masterscript = masterscript,
+               rawscript = rawscript, scorescript = scorescript,
+               mergescript = mergescript, path = path)
 }
