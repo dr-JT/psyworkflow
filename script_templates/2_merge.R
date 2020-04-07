1dff6ce72c8c56b29a1b94de3175fc566f29b85e
Jason Tsukahara <tsuky19@gmail.com>
2020-04-07 14:55:49 -0400

    update

--- script_templates/2_merge.R
+++ script_templates/2_merge.R
@@ -5,12 +5,13 @@ library(datawrangling) # to use files_join() and trim()
 library(dplyr)
 
 ## Set import/output directories
-import.dir <- "Data Files/Scored Data"
-output.dir <- "Data Files"
+import_dir <- "Data Files/Scored Data"
+output_dir <- "Data Files"
+output_file <- "name_of_datafile.csv"
 ################
 
 #### Import Files ####
-import <- files_join(here(import.dir), pattern = "Scores", id = "Subject")
+import <- files_join(here(import_dir), pattern = "Scores", id = "Subject")
 ######################
 
 #### Select only important variables and trim outlier scores ####
@@ -23,8 +24,8 @@ subj.list <- select(data_merge, Subject)
 #################################################################
 
 #### Output ####
-write_csv(data_merge, here(output.dir, "name_of_datafile.csv"))
-write_csv(subj.list, here(output.dir, "subjlist_final.csv"))
+write_csv(data_merge, here(output_dir, output_file))
+write_csv(subj.list, here(output_dir, "subjlist_final.csv"))
 ################
 
 rm(list=ls())
