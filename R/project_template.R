ef99fdaf623a576db710ddfb14272f08a2a08758
dr-JT <jtsukahara3@gatech.edu>
2021-11-22 13:16:06 -0500

    update

--- R/project_template.R
+++ R/project_template.R
@@ -1,16 +1,14 @@
 project_template <- function(path, script_templates, mainscript, rawscript,
                              scorescript, mergescript, study_doc, data_raw,
-                             data_scored, documents, results, tasks, sessions,
-                             other, standard_project) {
+                             data_scored, documents, results, tasks, sessions) {
   # ensure path exists
   dir.create(path, recursive = TRUE, showWarnings = FALSE)
 
   # create project
-  create_project(script_templates = script_templates,
+  create_project(path = path, script_templates = script_templates,
                  mainscript = mainscript, rawscript = rawscript,
                  scorescript = scorescript, mergescript = mergescript,
                  study_doc = study_doc, data_raw = data_raw,
                  data_scored = data_scored, documents = documents,
-                 results = results, tasks = tasks, sessions = sessions,
-                 other = c(), standard_project = standard_project, path = path)
+                 results = results, tasks = tasks, sessions = sessions)
 }
