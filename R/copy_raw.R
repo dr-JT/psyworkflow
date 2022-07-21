b71d74e0c653aebc6e63e2737de49a1501cc7f0d
Tsukahara <jtsukahara3@ipsec-10-2-192-179.vpn.gatech.edu>
2022-07-21 11:31:32 -0400

    update

--- R/copy_raw.R
+++ R/copy_raw.R
@@ -6,115 +6,111 @@
 #' @param to a directory where raw data files are to be copied to
 #' @param filetype a string pattern identifying a specific
 #'     type of raw data file (e.g. .edat2).
-#' @param remove logical. Should the files in the 'from' directory
-#'     be deleted? (Default: FALSE)
-#' @param subj.file a file path to a list of subjects who completed
-#'     the entire study. (Default: NULL).
-#' @param copy logical. Do you want to copy or only remove the files? (Default = TRUE)
-#' @param ext a directory extension to the "to" argument.
+#' @param sub_folder optional. do you want to copy files to a sub folder within
+#'     the `to = ` directory?
 #' @param task_dir.names Relationship between task name and task folder name.
 #'     Default is "numbered". Alternative options are; "asis".
+#' @param remove logical. Should the files in the 'from' directory
+#'     be deleted? (Default: FALSE)
+#' @param copy logical. Do you want to copy the files? (Default = TRUE)
 #' @export
 #'
 
-copy_raw <- function(from, to, filetype, remove = FALSE,
-                     subj.file = NULL, copy = TRUE, ext = NULL,
-                     task_dir.names = "numbered"){
+copy_raw <- function(from, to, filetype, sub_folder = NULL,
+                     task_dir.names = "numbered",
+                     remove = FALSE, copy = TRUE) {
 
   if (copy == TRUE) {
-    if (!is.null(subj.file)) {
-      delim <- ifelse(stringr::str_detect(subj.file, ".csv"), ",",
-                      ifelse(stringr::str_detect(subj.file, ".txt"), "\t", NULL))
-      if (delim == ",") {
-        subj.list <- readr::read_csv(subj.file)
-        colnames(subj.list) <- "Subject"
-      } else if (delim == "\t") {
-        subj.list <- readr::read_delim(subj.file, "\t",
-                                       escape_double = FALSE, trim_ws = TRUE)
-        colnames(subj.list) <- "Subject"
-      } else {
-        subj.list <- data.frame(Subject = NA)
-      }
-      subj.list <- as.character(subj.list$Subject)
-    }
 
-    dirs <- list.dirs(from, full.names = FALSE, recursive = FALSE)
-    for (task_dir in dirs){
-      files <- list.files(paste(from, task_dir, sep = "/"), pattern = filetype,
-                          recursive = TRUE, full.names = TRUE)
-      if (length(files)>0){
-        files.error <- c()
-        files.keep <- c()
-        files.drop <- c()
-        for (i in seq_along(files)){
-          if (!is.null(subj.file)) {
-            subj.complete <-
-              length(which(stringr::str_detect(files[i], subj.list) == TRUE))
+    if (!is.list(task_dir.names)) {
+      # get list of task folders
+      dirs <- list.dirs(from, full.names = FALSE, recursive = FALSE)
+      for (task_dir in dirs) {
+        # get list of data files within a task folder
+        files <- list.files(paste(from, task_dir, sep = "/"), pattern = filetype,
+                            recursive = TRUE, full.names = TRUE)
+
+        # only continue if there are files in the folder
+        if (length(files) > 0) {
+
+          # remove numbers or keep as is for new copied task folder
+          if (task_dir.names == "numbered") {
+            task <- strsplit(task_dir, "\\. ")[[1]][2]
+          }
+          if (task_dir.names == "asis") {
+            task <- task_dir
+          }
+          # extra stuff just making sure the task folder is named correctly
+          if (length(task[[1]]) == 1) {
+            task <- task[[1]][1]
           } else {
-            subj.complete <- 1
+            task <- task[[1]][2]
           }
-          error <-
-            length(which(stringr::str_detect(files[i], "conflicted copy") == TRUE))
-          if (error == 1) {
-            files.error <- c(files.error, files[i])
-          } else if (subj.complete == 1) {
-            files.keep <- c(files.keep, files[i])
-          } else if (subj.complete == 0) {
-            files.drop <- c(files.drop, files[i])
+          # create file paths for where to copy files
+          to_dir <- paste(to, task, sep = "/")
+          if (dir.exists(to_dir) == FALSE) {
+            dir.create(to_dir)
           }
-        }
-
-        if (task_dir.names == "numbered") {
-          task <-
-            paste(stringr::str_c(stringr::str_split(task_dir, " ")[[1]][-1]),
-                  sep = " ", collapse = "")
-        } else if (task_dir.names == "asis") {
-          task <- task_dir
-        }
-
-        if (length(task[[1]])==1){
-          task <- task[[1]][1]
-        } else {
-          task <- task[[1]][2]
-        }
-        copy_dir <- paste(to, task, sep = "/")
-        if (dir.exists(copy_dir) == FALSE){
-          dir.create(copy_dir)
-        }
-        if (!is.null(ext)) {
-          copy_dir <- paste(copy_dir, ext, sep = "/")
-          if (dir.exists(copy_dir) == FALSE){
-            dir.create(copy_dir)
+          # if needed, extend files paths to a sub folder
+          if (!is.null(sub_folder)) {
+            to_dir <- paste(to_dir, sub_folder, sep = "/")
+            if (dir.exists(to_dir) == FALSE) {
+              dir.create(to_dir)
+            }
           }
-        }
-        drop_dir <- paste(copy_dir, "Did not finish all sessions", sep = "/")
-        if (dir.exists(drop_dir) == FALSE) {
-          dir.create(drop_dir)
-        }
-        error_dir <- paste(copy_dir, "error", sep = "/")
-        if (dir.exists(error_dir) == FALSE) {
-          dir.create(error_dir)
-        }
+          # create a temporary directory for duplicates
+          dup_dir <- paste(to_dir, "/dup", sep = "")
+          if (dir.exists(dup_dir) == FALSE) {
+            dir.create(dup_dir)
+          }
 
-        file.copy(files.keep, copy_dir, copy.date = TRUE)
-        file.copy(files.drop, drop_dir, copy.date = TRUE)
-        file.copy(files.error, error_dir, copy.date = TRUE)
+          # check for duplicate file names in copy to directory
+          check_files <- list.files(to_dir, pattern = filetype,
+                                    recursive = FALSE, full.names = FALSE)
+          duplicated_n <- 0
+          for (file in files) {
+            # get file name and check for duplicates
+            filename <- strsplit(file, "/")[[1]][length(strsplit(file, "/")[[1]])]
+            duplicate <-
+              length(which(lapply(filename, grepl, x = check_files)[[1]] == TRUE))
+            if (duplicate >= 1) {
+              duplicated_n <- duplicated_n + 1
+              # copy file to temporary directory and rename as a duplicate
+              file.copy(file, dup_dir, copy.date = TRUE)
+              new_filename <-
+                paste("DUPLICATED (", duplicate, ") ", filename, sep = "")
+              file_from <- list.files(dup_dir, filename, full.names = TRUE)
+              file_to <- paste(dup_dir, new_filename, sep = "/")
+              file.rename(file_from, file_to)
+              # copy renamed file to copy to directory and remove from files
+              file.copy(file_to, to_dir, copy.date = TRUE)
+              file.remove(file_to)
+              files <- files[-which(file == files)]
+            }
+          }
 
-        cat("   ", "\n")
-        cat("-----------------------------------------", "\n")
-        cat("Files Copied: ", task, "\n")
-        cat("Completed Subjects: ", length(files.keep), "\n")
-        cat("Did not finish all sessions: ", length(files.drop), "\n")
-        cat("Files with errors: ", length(files.error), "\n")
-        cat("Total Files: ", length(files), "\n")
-        cat("-----------------------------------------", "\n")
+          unlink(dup_dir, recursive = TRUE)
 
-        if (remove == TRUE){
-          file.remove(files)
+          # copy over files
+          file.copy(files, to_dir, copy.date = TRUE)
+
+          cat("   ", "\n")
+          cat("-----------------------------------------", "\n")
+          cat("Files Copied: ", task, "\n")
+          cat("Duplicate Subject Files: ", duplicated_n, "\n")
+          cat("-----------------------------------------", "\n")
+
+          if (remove == TRUE) {
+            file.remove(files)
+          }
         }
       }
     }
-  } else {
+    if (is.list(task_dir.names)) {
+
+    }
+  }
+  else {
     if (remove == TRUE) {
       dirs <- list.dirs(from, full.names = FALSE, recursive = FALSE)
       for (task_dir in dirs) {
