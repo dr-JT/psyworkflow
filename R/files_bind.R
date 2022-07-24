b8ab1a6dc8f894407d567feccb6f4875897a275b
Jason Tsukahara <jtsukahara3@gatech.edu>
2022-07-24 11:42:30 -0400

    add error messaging

--- R/files_bind.R
+++ R/files_bind.R
@@ -5,13 +5,13 @@
 #' @param path Folder location of files to be binded
 #' @param pattern Pattern to identify files to be binded
 #' @param delim Delimiter used in files. Passed onto `readr::read_delim()`
+#' @param bind Bind "rows" or "columns"?
 #' @param output_file File name and path to be saved to.
 #' @export
 #'
 
-files_bind <- function(path = "", pattern = "", delim = ",",
-                       output_file = "",
-                       bind = "rows") {
+files_bind <- function(path = "", pattern = "", delim = ",", bind = "rows",
+                       output_file = "") {
 
   filelist <- list.files(path = path, pattern = pattern, full.names = TRUE)
   import <- list()
@@ -21,10 +21,18 @@ files_bind <- function(path = "", pattern = "", delim = ",",
     }
 
     if (delim != ",") {
-      import[[i]] <- readr::read_delim(filelist[[i]],
-                                       delim,
-                                       escape_double = FALSE,
-                                       trim_ws = TRUE)
+      import[[i]] <- tryCatch(
+        {
+          readr::read_delim(filelist[[i]],
+                            delim,
+                            escape_double = FALSE,
+                            trim_ws = TRUE)
+        },
+        error = function(err) {
+          message("Could not read in data file: ", filelist[[i]])
+          return(NA)
+        }
+      )
     }
   }
 
