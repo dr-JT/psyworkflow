3082dbd54c7fbaa62114cd05c87d979775ae7bee
Jason Tsukahara <jtsukahara3@gatech.edu>
2022-07-24 12:42:53 -0400

    add eprime support

--- R/files_bind.R
+++ R/files_bind.R
@@ -7,32 +7,47 @@
 #' @param delim Delimiter used in files. Passed onto `readr::read_delim()`
 #' @param bind Bind "rows" or "columns"?
 #' @param output_file File name and path to be saved to.
+#' @param source If data file comes from a specific software specify it here
+#'     for better support. Currently supports "eprime".
 #' @export
 #'
 
 files_bind <- function(path = "", pattern = "", delim = ",", bind = "rows",
-                       output_file = "") {
+                       output_file = "", source = "") {
 
   filelist <- list.files(path = path, pattern = pattern, full.names = TRUE)
   import <- list()
   for (i in seq_along(filelist)) {
-    if (delim == ",") {
-      import[[i]] <- readr::read_csv(filelist[[i]])
-    }
-
-    if (delim != ",") {
+    if (source == "eprime") {
+      delim <- "\t"
       import[[i]] <- tryCatch(
         {
           readr::read_delim(filelist[[i]],
                             delim,
                             escape_double = FALSE,
-                            trim_ws = TRUE)
+                            trim_ws = TRUE,
+                            na = "NULL")
         },
         error = function(err) {
-          message("Could not read in data file: ", filelist[[i]])
-          return(NA)
+          uni <- readr::read_delim(filelist[[i]],
+                                   locale = readr::locale(encoding = "UCS-2LE"),
+                                   delim,
+                                   escape_double = FALSE,
+                                   trim_ws = TRUE,
+                                   na = "NULL")
         }
       )
+    }
+
+    if (delim == ",") {
+      import[[i]] <- readr::read_csv(filelist[[i]])
+    }
+
+    if (delim != "," & source == "") {
+      import[[i]] <- readr::read_delim(filelist[[i]],
+                                       delim,
+                                       escape_double = FALSE,
+                                       trim_ws = TRUE)
     }
   }
 
